af563e0161eaebafefd961db77888587ec5360f2
A-ri-SIM <149152400+A-ri-SIM@users.noreply.github.com>
2024-10-08 19:37:44 +0900

    Setting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './page/NotFound';
+import Home from './page/Home';
+import Woman from './page/Woman';
+import Man from './page/Man';
+import ProductDetail from './page/ProductDetail';
+import MyCart from './page/MyCart';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/products/woman', element: <Woman /> },
+      { path: '/products/man', element: <Man /> },
+      { path: '/products/:id', element: <ProductDetail /> },
+      { path: '/cart', element: <MyCart /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
---
+++ src/page/Woman.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Woman() {
+    return (
+        <div>
+          Woman  
+        </div>
+    );
+}
+
